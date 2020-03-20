c281574594ef71cbd55623e56693247be37f671d
Nicola V <info@nicolavolpini.net>
2020-03-20 12:04:45 +0100

    Refactor code to use function

--- remember_me.py
+++ remember_me.py
@@ -2,13 +2,18 @@ import json
 
 filename = 'rememberme.json'
 
-try:
-    with open(filename) as f:
-        username = json.load(f)
-except FileNotFoundError:
-    username = input("what is your name? ")
-    with open(filename, 'w') as f:
-        json.dump(username,f)
-        print(f"We will remember you when you come back, {username}!")
-else:
-    print(f"Welcome back, {username}!")
+
+def greet_user():
+    try:
+        with open(filename) as f:
+            username = json.load(f)
+    except FileNotFoundError:
+        username = input("what is your name? ")
+        with open(filename, 'w') as f:
+            json.dump(username,f)
+            print(f"We will remember you when you come back, {username}!")
+    else:
+        print(f"Welcome back, {username}!")
+
+
+greet_user()
