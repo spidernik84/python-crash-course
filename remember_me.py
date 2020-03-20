0a8d30a597e6eea0879721626a7d8056dd436b65
Nicola V <info@nicolavolpini.net>
2020-03-20 12:15:04 +0100

    final refactor

--- remember_me.py
+++ remember_me.py
@@ -1,8 +1,5 @@
 import json
 
-
-
-
 def get_stored_username():
     """ Get user if available """
     filename = 'rememberme.json'
@@ -14,16 +11,21 @@ def get_stored_username():
     else:
         return username
 
+def ask_username():
+    username = input("what is your name? ")
+    filename = 'rememberme.json'
+    with open(filename, 'w') as f:
+        json.dump(username,f)
+    return username
+
+
 def greet_user():
     
     username = get_stored_username()
     if username:
         print(f"Hello {username}!")
     else:
-        username = input("what is your name? ")
-        filename = 'rememberme.json'
-        with open(filename, 'w') as f:
-            json.dump(username,f)
-            print(f"We will remember you when you come back, {username}!")
+        username = ask_username()
+        print(f"We will remember you when you come back, {username}!")
 
 greet_user()
